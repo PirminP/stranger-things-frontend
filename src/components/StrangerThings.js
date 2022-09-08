12293392d20ba644bf7b4db88d91b4f041943682
PirminP <pirmin.pernsteiner@gmail.com>
2022-09-07 23:22:13 -0300

    feat:verifying environment variabels of front-end

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -6,14 +6,19 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
 );
 
+const hawkins = process.env.REACT_APP_HAWKINS_URL;
+const updsidedown = process.env.REACT_APP_UPSIDEDOWN_URL;
+const hawkinsTimeout = process.env.REACT_APP_HAWKINS_TIMEOUT;
+const updsidedownTimeout = process.env.REACT_APP_UPSIDEDOWN_TIMEOUT;
+
 const strangerThingsConfig = {
-  url: 'http://localhost:3002',
-  timeout: 30000,
+  url: hawkins,
+  timeout: hawkinsTimeout,
 };
 
 const upsideDownConfig = {
-  url: 'http://localhost:3003',
-  timeout: 30000,
+  url: updsidedown,
+  timeout: updsidedownTimeout,
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
